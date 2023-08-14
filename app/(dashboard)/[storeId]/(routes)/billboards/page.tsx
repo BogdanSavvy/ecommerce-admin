f241ca2f1caa4110abe035e9922030eff9d7e774
BogdanSavvy <bogdansavin315@gmail.com>
2023-08-14 14:38:26 +0300

    Created a Billboards page, where you can add, delete and update billboards

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,9 @@
+import { BillboardClient } from "./components/client";
+
+const Billboards = () => {
+  return <div className="flex-cols space-y-4 p-8 pt-6">
+   <BillboardClient />
+  </div>;
+};
+
+export default Billboards;
---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
+import { CldUploadWidget } from "next-cloudinary";
+
+import { Button } from "@/components/ui/button";
+
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+export const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}) => {
+  const [isMounted, setIsMonted] = useState(false);
+
+  useEffect(() => {
+    setIsMonted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative h-[200px] w-[200px] overflow-hidden rounded-md"
+          >
+            <div className="absolute right-2 top-2 z-10">
+              <Button
+                type="button"
+                variant="destructive"
+                size="icon"
+                onClick={() => onRemove(url)}
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" src={url} alt="image" />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="f01i4f7k">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+          return (
+            <Button
+              type="button"
+              variant="secondary"
+              disabled={disabled}
+              onClick={onClick}
+            >
+              <ImagePlus className="mr-2 h-4 w-4" />
+              Upload an image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+};
