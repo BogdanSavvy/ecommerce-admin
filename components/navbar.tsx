ddeeb36dbe214193c8e62df95823b720ca7da7f3
BogdanSavvy <bogdansavin315@gmail.com>
2023-08-24 15:25:35 +0300

    Added theme-provider and theme-toggle to choose theme

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,8 @@ import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/main-nav";
-import StoreSwitcher from "@/components/store-swither";
+import { ThemeToggle } from "@/components/theme-toggle";
+import StoreSwitcher from "@/components/store-switcher";
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
@@ -25,6 +26,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
--- components/overview.tsx
+++ components/overview.tsx
@@ -29,4 +29,3 @@ export const Overview: React.FC<OverviewProps> = ({ data }) => {
     </ResponsiveContainer>
   );
 };
-
